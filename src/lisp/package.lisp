53d83f7b93d67da6a11a95d0af271f7e26535077
Adrian Medina <adrian.medina@mail.yu.edu>
2015-07-12 16:41:43 -0400

    initialize Objective-C frameworks

--- src/lisp/package.lisp
+++ src/lisp/package.lisp
@@ -1,2 +1,9 @@
 (defpackage :metal
   (:use :common-lisp))
+
+(defvar *frameworks*
+  '("/System/Library/Frameworks/Metal.framework/Metal"
+    "/System/Library/Frameworks/MetalKit.framework/MetalKit"
+    "/System/Library/Frameworks/ModelIO.framework/ModelIO"))
+
+(objc:ensure-objc-initialized :modules *frameworks*)
