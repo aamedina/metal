74be921fb82c09372c1e935d11095048b9ce9dea
Adrian Medina <adrian.medina@mail.yu.edu>
2015-07-12 17:59:58 -0400

    define higher-order metal-kit-frame initializer

--- src/lisp/capi.lisp
+++ src/lisp/capi.lisp
@@ -8,8 +8,12 @@
 (objc:define-objc-method ("initWithFrame:" objc:objc-object-pointer)
     ((self metal-kit-view)
      (frame cocoa:ns-rect))
+  (declare (ignore self))
   (when-let (self (objc:invoke (objc:current-super) "initWithFrame:" frame))
     (objc:invoke self "setDevice:" (create-system-default-device))
+    ;; MTLPixelFormatBGRA8Unorm_sRGB
+    (objc:invoke self "setColorPixelFormat:" 81) 
+    (objc:invoke self "setPreferredFramesPerSecond:" 60)
     self))
 
 (objc:define-objc-method ("drawRect:" :void)
@@ -21,3 +25,13 @@
     ((self metal-kit-view)
      (size cocoa:ns-size))
   (objc:invoke (objc:current-super) "setFrameSize:" size))
+
+(defun metal-kit-view-initializer (frame)
+  (lambda (pane view)
+    (declare (ignore pane))
+    (objc:invoke view "initWithFrame:" frame)))
+
+(defun make-metal-kit-view-pane (&key (frame #(0 0 1280 800)))
+  (make-instance 'capi:cocoa-view-pane
+                 :view-class "CLMTKView"
+                 :init-function (metal-kit-view-initializer frame)))
